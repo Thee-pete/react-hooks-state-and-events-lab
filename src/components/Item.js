233b92c352bf6c3f7357d815e6cdde97fc8c94b1
Peter Ndumia <ndegwandumia@gmail.com>
2022-07-19 12:58:33 +0300

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+ 
+  const [notInCart, setnotInCart] = useState(true);
+  function handleOnClick(){
+    setnotInCart(!notInCart);
+   
+
+  }
+  const appClass = notInCart ? "" : "in-cart";
+  const msg = notInCart ? "Add to Cart" : "Remove From Cart";
+
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleOnClick}>{msg}</button>
     </li>
   );
 }
